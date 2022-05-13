57410f7c0fca7dee240ca12d43d9d8fd026b5fc1
Fr75s <Fr75s@protonmail.com>
2022-05-13 11:16:47 -0500

    Trying Pages Again

---
+++ assets/SAFELY_REMOVABLE/website/commonelements.js
@@ -0,0 +1,23 @@
+// This just initializes the header for each page.
+
+document.getElementById("header").innerHTML = `
+<div class="row edgerow header">
+	<p style="margin: 10px; margin-left: 30px; font-family: Outfit; font-weight: 600;">Library</p>
+	<div class="row">
+		<a href="main.html" class="rowitem hlink">Home</a>
+		<a href="gallery.html" class="rowitem hlink">Gallery</a>
+		<a href="install.html" class="rowitem hlink">Installation</a>
+
+	</div>
+</div>
+`;
+
+/*
+document.getElementById("footer").innerHTML = `
+<div class="row edgerow">
+	<p style="margin: 30px; font-family: Outfit; font-weight: 600;">Theme & Website by Fr75s.</p>
+
+	<a href="https://github.com/Fr75s/library" class="rowitem" style="margin: 30px;">Github Page</a>
+</div>
+`;
+*/
