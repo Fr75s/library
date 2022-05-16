5fe511465c420f96a6f9df98b2879e00388c1658
Fr75s <Fr75s@protonmail.com>
2022-05-16 12:45:44 -0500

    Added footer to website

--- assets/SAFELY_REMOVABLE/website/commonelements.js
+++ assets/SAFELY_REMOVABLE/website/commonelements.js
@@ -7,17 +7,16 @@ document.getElementById("header").innerHTML = `
 		<a href="main.html" class="rowitem hlink">Home</a>
 		<a href="gallery.html" class="rowitem hlink">Gallery</a>
 		<a href="install.html" class="rowitem hlink">Installation</a>
-
 	</div>
 </div>
 `;
 
-/*
+
 document.getElementById("footer").innerHTML = `
 <div class="row edgerow">
-	<p style="margin: 30px; font-family: Outfit; font-weight: 600;">Theme & Website by Fr75s.</p>
+	<p style="margin: 30px; font-family: Outfit; font-weight: 600;">Theme & Website by <a href="https://github.com/Fr75s/">Fr75s.</a></p>
 
-	<a href="https://github.com/Fr75s/library" class="rowitem" style="margin: 30px;">Github Page</a>
+	<a href="https://github.com/Fr75s/library" class="rowitem" style="margin: 30px;">Source Code</a>
 </div>
 `;
-*/
+
